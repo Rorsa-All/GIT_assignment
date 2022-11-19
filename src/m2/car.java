1a9aa3d27ed917af942568aae8ea61765971da76
Rorsa-All <my.journey.h@gmail.com>
2022-11-19 16:42:44 +0300

    Initial commit

---
+++ src/m2/car.java
@@ -0,0 +1,31 @@
+package m2;
+
+public class car {
+	
+	public static void main(String[] args)
+	{
+		brand car1 = new brand(20,"Nissan");
+		System.out.println(car1.name + " is not bad mark which is " + car1.year + " years old");
+		
+		brand car2 = new brand();
+		System.out.println(car2.year+" years");
+		
+		car2.say("Tesla");
+		
+	}
+}
+      class brand
+      {
+    	  public String name= "Default";
+    	  public int year = 10;
+    	  
+    	  public brand(int g, String n)
+    	  {   year = g;
+    		  name = n;
+    	  }
+    	  public void say(String n)
+    	  {
+    		  System.out.println(n+" is bad car");
+    	  }
+    	  public brand() {}
+      }
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author ASROR
+ *
+ */
+module m2 {
+}
